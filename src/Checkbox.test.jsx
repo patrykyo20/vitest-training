e09176a0dc0c9500f9c337346ec224f080d147f3
Patryk Wierzbowski <pwierzbowski@sii.pl>
2024-08-12 08:33:52 +0200

    feat: create Checkbox component & tests

---
+++ src/Checkbox.test.jsx
@@ -0,0 +1,17 @@
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import CheckboxWithLabel from './Checkbox.tsx';
+import { afterEach, expect, it } from "vitest";
+
+afterEach(cleanup);
+
+it('CheckboxWithLabel changes the text after click', () => {
+  const {queryByLabelText, getByLabelText} = render(
+    <CheckboxWithLabel labelOn="On" labelOff="Off" />,
+  );
+
+  expect(queryByLabelText(/off/i)).toBeTruthy();
+
+  fireEvent.click(getByLabelText(/off/i));
+
+  expect(queryByLabelText(/on/i)).toBeTruthy();
+});
